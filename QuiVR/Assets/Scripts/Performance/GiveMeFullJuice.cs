9d26272533f2f2bbc950c82e81fa4e382d9f406e
Roberto <roberto.coviello@bearingpoint.com>
2022-10-26 09:19:45 +0200

    Install Oculus XR Plugin

---
+++ QuiVR/Assets/Scripts/Performance/GiveMeFullJuice.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using Unity.XR.Oculus;
+
+namespace Performance
+{
+    public class GiveMeFullJuice : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // OVRManager.cpuLevel = 2;
+            // OVRManager.gpuLevel = 2;
+        }
+    }
+}
